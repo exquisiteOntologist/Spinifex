5a9457bf28f423b3cefe2df86092fb8ddb993b4f
Jon Vaillant <hi@jonvaillant.com>
2023-02-18 23:16:11 +0800

    Persisting grass positions

--- field-a/src/app.ts
+++ field-a/src/app.ts
@@ -3,45 +3,89 @@ const cW = mainCanvas.width = window.innerWidth
 const cH = mainCanvas.height = cW * 0.56
 const mainContext = mainCanvas.getContext('2d')
 
-// const renderCanvas = document.
-
 const drawBackground = (ctx: CanvasRenderingContext2D) => {
     ctx.fillStyle = '#0e0e0e'
     ctx.fillRect(0, 0, cW, cH)
 }
 
 const deviate = (num: number, up: number, low: number): number => {
-    const ran = Math.random()
-    const mid = ran + 0.5
-    const deviation = num * mid
-    if (deviation > num * up) return deviate(num, up, low)
-    if (deviation < num * low) return deviate(num, up, low)
+    const ran = Math.random() // 0.0 to 1.0
+    const multi = ran + 0.5 // 0.5 to 1.5
+    const deviation = num * multi
+    if (deviation > (num * up)) return deviate(num, up, low)
+    if (deviation < (num * low)) return deviate(num, up, low)
     return deviation
 }
 
-const flip = (): number => Math.random() > 0.5 ? 1 : -1
+const flip = (): -1 | 1 => Math.random() > 0.5 ? 1 : -1
 
-const drawGrassBlade = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-    const xF = flip()
-    const h = deviate(120, 2, 0.3)
-    const x1 = x + (5 * xF)
-    const x2 = x + (35 * xF)
-    const y1 = y - (h/2)
-    const y2 = y - h
+interface GrassBlade {
+    /** Direction multiplier (-1 OR 1) */
+    xF: -1 | 1,
+    /** Height */
+    h: number,
+    x: number,
+    y: number,
+    /** Mid-point X */
+    x1: number,
+    x2: number,
+    /** Mid-point Y */
+    y1: number,
+    y2: number
+    /** Colour */
+    c: string,
+    /** Line Width */
+    lW: number
+}
+
+const drawGrassBlade = (ctx: CanvasRenderingContext2D, b: GrassBlade) => {
+    const shift = deviate(1, 3, -3)
+    // b.x1 += shift 
+    // b.x2 += shift
+    
     ctx.beginPath()
-    ctx.strokeStyle = '#fff'
-    ctx.lineWidth = 1
-    ctx.moveTo(x, y)
-    ctx.arcTo(x1, y1, x2, y2, 40)
+    ctx.strokeStyle = b.c
+    ctx.lineWidth = b.lW
+    ctx.moveTo(b.x, b.y)
+    ctx.arcTo(b.x1, b.y1, b.x2, b.y2, 40)
     ctx.stroke()
     ctx.closePath()
 }
 
+const createGrassBlade = (x: number, y: number): GrassBlade => {
+    const dX: number = deviate(x, 1.5, 0.5)
+    const xF = flip()
+    const h = deviate(120, 2, 0.3)
+    const rgb = 250 + (deviate(5, 1, -1) * flip())
+    const alpha = 0.5 + deviate(0.25, 1.5, 0.5)
+
+    const b: GrassBlade = {
+        xF,
+        h,
+        x: dX,
+        y,
+        x1: dX + (5 * xF),
+        x2: dX + (35 * xF),
+        y1: y - (h/2),
+        y2: y - h,
+        c: `rgba(${rgb}, ${rgb}, ${rgb}, ${alpha})`,
+        lW: 1
+    }
+
+    return b
+}
+
+const numBlades = 600
+const grassBlades: GrassBlade[] = []
 const drawGrassBlades = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-    const numBlades = 30
+    if (grassBlades.length <= numBlades) {
+        for (let i = 0; i < numBlades; i++) {
+            grassBlades.push(createGrassBlade(x, y))
+        }
+    }
+
     for (let i = 0; i < numBlades; i++) {
-        const dX: number = deviate(x, 1.5, 0.5)
-        drawGrassBlade(ctx, dX, y)
+        drawGrassBlade(ctx, grassBlades[i])
     }
 }
 
